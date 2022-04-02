855b50470fec5106e31d2c4ab03b32bf799a0ce2
RicafrenteErica <acirecordovez@gmail.com>
2022-04-02 15:07:05 +0800

    My Hobbies added

--- git-python-Ricafrente.py
+++ git-python-Ricafrente.py
@@ -4,3 +4,8 @@ print{"April 17, 2001"}
 print{"MSC"}
 print{"BSI/T"}
 print{"2-E"}
+print{"reading a book"}
+print{"sleeping"}
+print{"eating"}
+print{"watching a movie"}
+print{"playing mobile games"}
